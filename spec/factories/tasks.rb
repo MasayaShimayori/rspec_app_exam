f058b5692bf4e11474b68a70b9c470f648642f73
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2021-03-23 20:02:35 +0900

    change variant name of done_task(trait)

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     association :project
   end
 
-  trait :noupdate_day do
+  trait :done do
     status { :done }
     completion_date { Time.current.yesterday }
   end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -81,7 +81,7 @@ RSpec.describe 'Task', type: :system do
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        task = create(:task, :noupdate_day)
+        task = create(:task, :done)
         visit edit_project_task_path(project, task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
